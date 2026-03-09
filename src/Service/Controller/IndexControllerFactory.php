4ca8c2941d789d4561696d84c7dfcc902b2440cc
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-03-09 00:00:00 +0000

    Replaced Interop\Container by standard Psr\Container.

--- src/Service/Controller/IndexControllerFactory.php
+++ src/Service/Controller/IndexControllerFactory.php
@@ -2,7 +2,7 @@
 
 namespace Urify\Service\Controller;
 
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Urify\Controller\Admin\IndexController;
 
--- src/Service/Form/UrifyFormFactory.php
+++ src/Service/Form/UrifyFormFactory.php
@@ -2,7 +2,7 @@
 
 namespace Urify\Service\Form;
 
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Urify\Form\UrifyForm;
 
